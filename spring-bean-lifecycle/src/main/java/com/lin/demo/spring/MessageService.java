c68f23a5eee17c89e1908f3540851207475b7326
michael <michael@local-michael>
2017-04-28 09:36:15 +0800

    init

---
+++ spring-bean-lifecycle/src/main/java/com/lin/demo/spring/MessageService.java
@@ -0,0 +1,7 @@
+package com.lin.demo.spring;
+
+public interface MessageService {
+	
+	String getMessage();
+
+}
---
+++ spring-bean-lifecycle/src/test/java/com/lin/demo/spring/MessagePrinterTest.java
@@ -0,0 +1,14 @@
+package com.lin.demo.spring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessagePrinterTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
